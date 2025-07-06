2af8b9dd545ca5e957f8525fb771f4ed79c2ec64
Kamdemherman <kamdemherman9@gmail.com>
2025-07-07 02:20:56 +0800

    config vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import serverless from 'serverless-http';
 
 // Import des routes
-import clientRouter from '../routes/client.js';
-import invoiceRouter from '../routes/invoice.js';
-import paymentRouter from '../routes/payment.js';
-import productRouter from '../routes/product.js';
+import clientRouter from '../routes/clients.js';
+import invoiceRouter from '../routes/invoices.js';
+import paymentRouter from '../routes/payments.js';
+import productRouter from '../routes/products.js';
 import remindersRouter from '../routes/reminders.js';
 import settingsRouter from '../routes/settings.js';
 import emailRouter from '../routes/email.js';
